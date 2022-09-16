b7cb06b4f9ea4a581313741474b7af6cdd08c724
yuvahree <107060025+yuvahree@users.noreply.github.com>
2022-09-16 19:16:52 +0530

    updated now

---
+++ pages/lib/navLinks.js
@@ -0,0 +1,15 @@
+const navLinks = [
+  { name: "Home", path: "/" },
+  {
+    name: "About us",
+    path: "/posts/[id]",
+  },
+  {
+    name: "Services",
+    path: "/posts/user",
+  },
+  {
+    name: "Blog",
+    path: "/blog",
+  },
+];
